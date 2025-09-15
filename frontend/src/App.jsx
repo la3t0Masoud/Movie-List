5f0cc95d9a66a2e5b86892f2fd65358f9b647d1e
la3t0Masoud <la3t.masoud11@gmail.com>
2025-09-15 16:19:31 +0330

    styles are updated and api is seted and serchbar is seted by api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,27 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './css/App.css'
 import Home from './pages/Home'
 import {Routes , Route} from 'react-router-dom'
 import Favorites from './pages/Favorites'
+import NavBar from './components/NavBar'
+import './css/App.css'
 
 
 
 
 function App() {
   return (
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/favorites" element={<Favorites/>}/>
-      </Routes>
-    </main>
+    <div>
+      <NavBar/>
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/favorites" element={<Favorites/>}/>
+        </Routes>
+      </main>
+    </div>
   )
 }
 export default App
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,30 +1,48 @@
 import React from 'react'
+import {getPopularMovies,searchMovies} from '../services/api'
 import MovieCard from "../components/MovieCard"
-import { useState } from 'react';
-
+import {useState ,useEffect  } from 'react';
+import '../css/Home.css'
 
 function Home() {
 
     const[searchQuery , setSearchQuery] = useState("");
+    const[movies, setMovies] = useState([]);
+    const[error, setError] = useState(null);
+    const[loading, setLoading] = useState(true)
 
-    const movies = [{
-        id:1,
-        title: "John Wick",
-        release_date:"2020"
-    },{
-        id:2,
-        title: "Matrix",
-        release_date:"1998"
-    },{
-        id:3,
-        title: "Terminator",
-        release_date:"1999"
-    }];
+    useEffect(()=>{
+        const loadPopularMovies = async ()=>{
+            try{
+                const popularMovies = await getPopularMovies()
+                setMovies(popularMovies)
+            } catch(err){
+                console.log(err)
+                setError("Failed to load movies...")
+            }
+            finally{
+                setLoading(false)
+            }
+        }
+        loadPopularMovies()
+    },[])
 
-    const handleSearch = (e) =>{
+    const handleSearch = async(e) =>{
         e.preventDefault()
-        alert(searchQuery);
-    }
+        if(!searchQuery.trim()) return
+        if(loading) return
+        setLoading(true)
+        try{
+            const searchResaults = await searchMovies(searchQuery)
+            setMovies(searchResaults)
+            setError(null)
+        }catch(err){
+            console.log(err)
+            setError("Failed to Search movies...")
+        }finally{
+            setLoading(false)
+        }
+    };
 
   return (
     <div className='home'>
@@ -38,12 +56,18 @@ function Home() {
             />
             <button type='submit' className="search-button">Search</button>
         </form>
-      <div className="movie-grid">
-        {movies.map((movie) => (
-            movie.title.toLowerCase().startsWith(searchQuery) && 
+        {error && <div className="error-message">{error}</div>}
+        {loading 
+        ? (<div className="loading">
+            Loading...
+        </div>
+        ):(<div className="movie-grid">
+            {movies.map((movie) => (
             <MovieCard movie={movie} key={movie.id} />
             ))}
-      </div>
+        </div>
+        )}
+      
     </div>
   )
 }
