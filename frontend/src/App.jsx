92d5a3b648abb9c67eb05d6e74f110799021ee34
la3t0Masoud <la3t.masoud11@gmail.com>
2025-09-15 13:30:43 +0330

    routes seted up

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './pages/Home'
+import {Routes , Route} from 'react-router-dom'
+import Favorites from './pages/Favorites'
+
+
+
+
 function App() {
   return (
-    <>
-    <Home/>
-    </>
+    <main className="main-content">
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/favorites" element={<Favorites/>}/>
+      </Routes>
+    </main>
   )
 }
 export default App
---
+++ frontend/src/pages/Favorites.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Favorites() {
+  return (
+    <div className='favorites-empty'>
+      <h2>
+        No Favorite Movies Yet :|
+      </h2>
+      <p>
+        Start adding movies to your favorited=s and they will apear here!
+      </p>
+    </div>
+  )
+}
+
+export default Favorites
